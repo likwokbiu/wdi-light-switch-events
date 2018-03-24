d3d8a3c0840c3808178f9d65ed6ac9967946a61b
likwokbiu <li_bill@ymail.com>
2018-03-24 16:26:52 -0400

    click listen added and worked

--- js/main.js
+++ js/main.js
@@ -1,2 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+  pageBody = document.querySelector('body');
+  pageSwitch = pageBody.querySelector('.switch');
+  pageStatus = pageBody.querySelector('.status');
 
+  switchOff();
 
+  pageSwitch.addEventListener('click', switchChange);
+
+  function switchChange() {
+    if(pageBody.className === 'dark') {
+      switchOn();
+    } else {
+      switchOff();
+    }
+  };
+
+  function switchOn() {
+    pageBody.className = 'light';
+    pageSwitch.classList.remove("off");
+    pageSwitch.classList.add("on");
+    pageStatus.innerText = 'It\'s so bright in here!';
+  };
+
+  function switchOff() {
+    pageBody.className = 'dark';
+    pageSwitch.classList.remove("on");
+    pageSwitch.classList.add("off");
+    pageStatus.innerText = 'Hey, who turned off the lights?';
+  };
+
+});
